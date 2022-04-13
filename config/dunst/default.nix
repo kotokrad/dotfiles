7ea3e8cfc6c191d54eb173544da334268a40b7f6
kotokrad <dsl2407@gmail.com>
2022-04-13 12:29:53 +0700

    Change Dunst actions

--- config/dunst/default.nix
+++ config/dunst/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   services.dunst = {
@@ -21,7 +21,9 @@
         line_height = 4;
         format = ''<b>%s</b>\n%b'';
         dmenu = "${pkgs.rofi}/bin/rofi -dmenu";
-        mouse_right_click = "context";
+        mouse_left_click = "close_current";
+        mouse_right_click = "do_action";
+        mouse_middle_click = "context";
       };
     };
   };
